c6806ac0014bba2e9a1324508d0905439d85b6dd
Matthew Higgins <matthew.alan.higgins@gmail.com>
2016-04-04 19:44:30 -0700

    Initial commit

---
+++ spec/ttt_spec.rb
@@ -0,0 +1,143 @@
+require_relative '../ttt'
+
+RSpec.describe Board do
+  board = Board.new
+  it "starts off with nine empty board spaces" do
+    expect(board.grid.length).to eq(9)
+    expect(board.grid.join('').empty?).to eq(true)
+  end
+
+  it "has no winner before a game is played/by default" do
+    expect(board.winner).to eq(false)
+  end
+
+  describe '#mark_board' do
+    it "marks the board at the given spot with the given marker" do
+      board = Board.new
+      random_number = rand(9)
+      board.mark_board(random_number, Board::X_MARKER)
+      expect(board.grid[random_number]).to eq(Board::X_MARKER)
+    end
+  end
+
+  describe '#check_if_winner' do
+    it "returns false if there is no winner" do
+      board = Board.new
+      expect(board.check_if_winner(Board::X_MARKER)).to eq(false)
+    end
+
+    it "returns the winner's marker if there is a winner" do
+      board = Board.new
+      random_winning_combo = Board::WINNING_COMBOS[rand(Board::WINNING_COMBOS.length)]
+      random_winning_combo.each do |index|
+        board.mark_board(index, Board::X_MARKER)
+      end
+      expect(board.check_if_winner(Board::X_MARKER)).to eq(Board::X_MARKER)
+    end
+  end
+
+  describe '#board_full?' do
+    it 'returns false for a new board' do
+      board = Board.new
+      expect(board.board_full?).to eq(false)
+    end
+
+    it 'returns false for a partially filled out board' do
+      board = Board.new
+      random_number_of_marks = rand(9)
+      random_indices = (0..8).to_a.sample(random_number_of_marks).sort
+      random_indices.each { |index| board.grid[index] = ([Board::X_MARKER, Board::O_MARKER].sample) }
+      expect(board.board_full?).to eq(false)
+    end
+
+    it 'returns true for a completely filled out board' do
+      board = Board.new
+      (0..9).to_a.each { |index| board.grid[index] = ([Board::X_MARKER, Board::O_MARKER].sample) }
+      expect(board.board_full?).to eq(true)
+    end
+  end
+
+  describe "#all_taken_spaces" do
+    it 'returns an empty array by default' do
+      board = Board.new
+      expect(board.all_taken_spaces.empty?).to eq(true)
+    end
+
+    it 'returns [0, 1, 2] if those spaces are hard-coded' do
+      board = Board.new
+      board.grid[0] = Board::X_MARKER
+      board.grid[1] = Board::O_MARKER
+      board.grid[2] = Board::X_MARKER
+      expect(board.all_taken_spaces).to eq([0, 1, 2])
+    end
+
+    it 'returns the correct indices for a randomly marked board' do
+      random_number_of_marks = rand(10)
+      random_indices = (0..8).to_a.sample(random_number_of_marks).sort
+      board = Board.new
+      random_indices.each { |index| board.grid[index] = ([Board::X_MARKER, Board::O_MARKER].sample) }
+      expect(board.all_taken_spaces).to eq(random_indices)
+    end
+  end
+
+  describe '#all_free_spaces' do
+    it 'returns a board with nine free spaces by default' do
+      board = Board.new
+      expect(board.all_free_spaces.length == 9).to eq(true)
+    end
+
+    it 'returns the correct number of free spaces for a board that has been played on' do
+      random_number_of_marks = rand(10)
+      random_indices = (0..8).to_a.sample(random_number_of_marks)
+      board = Board.new
+      random_indices.each { |index| board.grid[index] = ([Board::X_MARKER, Board::O_MARKER].sample) }
+      expect(board.all_free_spaces.length).to eq(9 - random_number_of_marks)
+    end
+
+    it 'correctly identifies which spaces are free' do
+      random_number_of_marks = rand(10)
+      random_indices = (0..8).to_a.sample(random_number_of_marks).sort
+      board = Board.new
+      (0..8).to_a.each { |index| board.mark_board(index, Board::X_MARKER) }
+      random_indices.each { |index| board.grid[index] = (Board::EMPTY_SPACE) }
+      expect(board.all_free_spaces).to eq(random_indices)
+    end
+  end
+
+  describe "#spaces_taken_by" do
+    it 'returns an empty array by default' do
+      board = Board.new
+      expect(board.spaces_taken_by(Board::X_MARKER).empty?).to eq(true)
+    end
+
+    it 'returns [0, 1, 2] if those spaces are hard-coded' do
+      board = Board.new
+      board.grid[0] = Board::X_MARKER
+      board.grid[1] = Board::X_MARKER
+      board.grid[2] = Board::X_MARKER
+      expect(board.spaces_taken_by(Board::X_MARKER)).to eq([0, 1, 2])
+    end
+
+    it 'returns the correct indices for a randomly marked board' do
+      random_number_of_marks = rand(10)
+      random_indices = (0..8).to_a.sample(random_number_of_marks).sort
+      board = Board.new
+      random_indices.each { |index| board.grid[index] = Board::X_MARKER }
+      expect(board.spaces_taken_by('x')).to eq(random_indices)
+    end
+  end
+
+  describe '#find_winning_indices' do
+    it 'returns an empty array for an empty board (e.g. a board without a win)' do
+      board = Board.new
+      marked_spaces = []
+      expect(board.find_winning_indices(marked_spaces).empty?).to eq(true)
+    end
+
+    it 'returns an array of the winning indices when the supplied marked spaces contain a winning combo' do
+      board = Board.new
+      marked_spaces = [0, 1, 2, 5, 6]
+      expect(board.find_winning_indices(marked_spaces)).to eq([0, 1, 2])
+    end
+  end
+end
---
+++ ttt.rb
@@ -0,0 +1,203 @@
+# represents game state for tie tac toe
+class Board
+  attr_reader :winner, :human_marker, :computer_marker, :computer_turns, :turn, :grid
+
+  EMPTY_SPACE = ''.freeze
+  X_MARKER = 'x'.freeze
+  O_MARKER = 'o'.freeze
+  WINNING_COMBOS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ].freeze
+
+  def initialize
+    @grid = [EMPTY_SPACE] * 9
+    @winner = false
+    @human_marker = X_MARKER
+    @computer_marker = (@human_marker == X_MARKER ? O_MARKER : X_MARKER)
+    @computer_turns = 0
+    @turn = human_marker
+  end
+
+  def deep_copy
+    Marshal::load(Marshal.dump(self))
+  end
+
+  def mark_board(index, marker)
+    @grid[index] = marker
+    @computer_turns += 1 if marker == @computer_marker
+    @turn = marker == @human_marker ? @computer_marker : @human_marker
+  end
+
+  def check_if_winner(marker)
+    marked_spaces = spaces_taken_by(marker)
+    find_winning_indices(marked_spaces).empty? ? false : @winner = marker
+  end
+
+  def board_full?
+    !@grid.include?(EMPTY_SPACE)
+  end
+
+  def find_spaces
+    @grid.each_index.select { |index| yield index } if block_given?
+  end
+
+  def all_taken_spaces
+    find_spaces { |index| @grid[index] == X_MARKER || @grid[index] == O_MARKER }
+  end
+
+  def all_free_spaces
+    find_spaces { |index| @grid[index] == EMPTY_SPACE }
+  end
+
+  def spaces_taken_by(marker)
+    find_spaces { |index| @grid[index] == marker }
+  end
+
+  def find_winning_indices(marked_spaces)
+    WINNING_COMBOS.select { |winning_combo| (marked_spaces & winning_combo).length == 3 }.flatten
+  end
+
+  def is_there_a_winner?
+    check_if_winner(X_MARKER) || check_if_winner(O_MARKER)
+  end
+
+  def stop_playing?
+    is_there_a_winner? || board_full?
+  end
+end
+
+# responsible for handling user input and updating game state for the human player
+class HumanPlayer
+  def find_move(board)
+    valid_play = false
+    until valid_play
+      move = gets.chomp.to_i - 1
+      valid_play = (0..8).include?(move) && (board.grid[move] == Board::EMPTY_SPACE)
+    end
+    move
+  end
+end
+
+# logic for calculating the optimal move updating game state for the computer player
+class ComputerPlayer
+  attr_reader :best_move
+
+  def find_move(board)
+    minimax(board)
+    @best_move.index
+  end
+
+  def minimax(board)
+    return score(board) if board.stop_playing?
+    moves = []
+    board.all_free_spaces.each do |space|
+      potential_board = board.deep_copy
+      moves << potential_move = PotentialMove.new(space, potential_board.turn)
+      potential_board.mark_board(potential_move.index, potential_move.marker)
+      potential_move.score = minimax(potential_board)
+    end
+    @best_move = choose_best_move(moves, board)
+    @best_move.score
+  end
+
+  def choose_best_move(moves, board)
+    if board.turn == board.computer_marker
+      moves.sort_by(&:score).reverse.first
+    else
+      moves.sort_by(&:score).first
+    end
+  end
+
+  def score(board)
+    if board.winner == board.human_marker
+      -5 + board.computer_turns
+    elsif board.winner == board.computer_marker
+      5 - board.computer_turns
+    else
+      0
+    end
+  end
+end
+
+# holds state about a potential move
+class PotentialMove
+  attr_accessor :score, :index, :marker
+  def initialize(index, marker)
+    @index = index
+    @marker = marker
+    @score = 0
+  end
+end
+
+# responsible for program/game flow
+class Game
+  attr_reader :board, :computer_player
+
+  def initialize
+    @board = Board.new
+    @human_player = HumanPlayer.new
+    @computer_player = ComputerPlayer.new
+    @ui = ConsoleUI.new
+  end
+
+  def play
+    player = @human_player
+    until @board.stop_playing?
+      @ui.display_board(@board)
+      @ui.display_instructions(@board.turn, @board.human_marker)
+      @board.mark_board(player.find_move(@board), @board.turn)
+      player = switch_players(player)
+    end
+    @ui.display_result(@board)
+  end
+
+  def switch_players(current_player)
+    current_player == @human_player ? @computer_player : @human_player
+  end
+end
+
+# console-based view layer
+class ConsoleUI
+  def display_board(board)
+    puts
+    board.grid.each_slice(3) do |slice|
+      slice.each { |space| space == Board::EMPTY_SPACE ? print("_ ") : print("#{space} ") }
+      puts
+    end
+    puts
+    puts "=" * 6
+  end
+
+  def display_instructions(current_marker, human_marker)
+    current_marker == human_marker ? display_human_instructions : display_computer_instructions
+  end
+
+  def display_human_instructions
+    puts "Your turn. Choose a space, 1-9, to play on the board."
+  end
+
+  def display_computer_instructions
+    puts "Computer's turn. He's thinking."
+    sleep(1.5)
+  end
+
+  def display_result(board)
+    display_board(board)
+    puts "Game Over"
+    if board.winner
+      board.winner == board.human_marker ? puts("You won!") : puts("The computer won :(")
+    else
+      puts "It's a draw"
+    end
+  end
+end
+
+first_game = Game.new
+first_game.play

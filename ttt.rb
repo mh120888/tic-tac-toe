4640d2d8dfd0e4eeb3254dccf877d4fff30c5bcd
Matthew Higgins <matthew.alan.higgins@gmail.com>
2016-04-17 08:25:49 -0700

    Removes typo

--- ttt.rb
+++ ttt.rb
@@ -103,7 +103,7 @@ class ComputerPlayer
     board.all_free_spaces.each do |space|
       potential_board = board.deep_copy
       moves << (potential_move = {space: space, score: 0, marker: potential_board.turn})
-      potential_boardgit.mark_board(potential_move[:space], potential_move[:marker])
+      potential_board.mark_board(potential_move[:space], potential_move[:marker])
       potential_move[:score] = minimax(potential_board)
     end
     @best_move = choose_best_move(moves, board)

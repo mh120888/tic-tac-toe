968df382c84ef5c76705499af77b3b1acebf5e89
Matthew Higgins <matthew.alan.higgins@gmail.com>
2016-04-17 11:04:52 -0700

    Updates ConsoleUI and HumanPlayer to accomodate differnt board sizes

--- ttt.rb
+++ ttt.rb
@@ -1,6 +1,6 @@
 # represents game state for tie tac toe
 class Board
-  attr_reader :winner, :human_marker, :computer_marker, :computer_turns, :turn, :grid, :winning_combos
+  attr_reader :winner, :human_marker, :computer_marker, :computer_turns, :turn, :grid, :winning_combos, :spaces_across
   attr_accessor :human_marker
 
   EMPTY_SPACE = ''.freeze
@@ -100,9 +100,9 @@ class HumanPlayer
     move = nil
     loop do
       move = gets.chomp.to_i - 1
-      valid_play = (0..8).include?(move) && (board.grid[move] == Board::EMPTY_SPACE)
+      valid_play = (0..board.spaces_across**2).include?(move) && (board.grid[move] == Board::EMPTY_SPACE)
       break if valid_play
-      puts "Please enter a number between 1 and 9 that has not already been taken"
+      puts "Please enter a number between 1 and #{board.spaces_across**2} that has not already been taken"
     end
     move
   end
@@ -163,10 +163,11 @@ class Game
     player = @ui.human_plays_first? ? @human_player : @computer_player
     human_marker = @ui.which_marker
     starting_marker = determine_starting_marker(player, human_marker)
-    @board = Board.new(human_marker, starting_marker, 4)
+    board_size = @ui.determine_board_size
+    @board = Board.new(human_marker, starting_marker, board_size)
     until @board.stop_playing?
       @ui.display_board(@board)
-      @ui.display_instructions(@board.turn, @board.human_marker)
+      @ui.display_instructions(@board.turn, @board.human_marker, @board)
       @board.mark_board(player.find_move(@board), @board.turn)
       player = switch_players(player)
     end
@@ -187,7 +188,7 @@ end
 class ConsoleUI
   def display_board(board)
     puts
-    board.grid.each_slice(3) do |slice|
+    board.grid.each_slice(board.spaces_across) do |slice|
       slice.each { |space| space == Board::EMPTY_SPACE ? print("_ ") : print("#{space} ") }
       puts
     end
@@ -195,17 +196,27 @@ class ConsoleUI
     puts "=" * 6
   end
 
-  def display_instructions(current_marker, human_marker)
-    current_marker == human_marker ? display_human_instructions : display_computer_instructions
+  def display_instructions(current_marker, human_marker, board)
+    current_marker == human_marker ? display_human_instructions(board) : display_computer_instructions
   end
 
-  def display_human_instructions
-    puts "Your turn. Choose a space, 1-9, to play on the board."
+  def display_human_instructions(board)
+    puts "Your turn. Choose a space, 1-#{board.spaces_across**2}, to play on the board."
   end
 
   def display_computer_instructions
     puts "Computer's turn. He's thinking."
     sleep(1.5)
+  end
+
+  def determine_board_size
+    board_size = 0
+    loop do
+      puts "What size board would you like to play? 3x3 (3) or 4x4 (4)?"
+      board_size = gets.chomp.to_i
+      break if (3..4).include?(board_size)
+    end
+    board_size
   end
 
   def human_plays_first?

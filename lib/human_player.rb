2e41a00c40dbe8c32ede8350f4fbbb9bb50fce20
Matt Higgins <matthew.alan.higgins@gmail.com>
2016-04-20 13:17:10 -0700

    Adds customizable input and output channels to HumanPlayer

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -1,16 +1,23 @@
 # responsible for handling user input and updating game state for the human player
 class HumanPlayer
+  attr_accessor :input, :output
+
+  def initialize
+    @input = $stdin
+    @output = $stdout
+  end
+
   def find_move(board, move=nil)
     loop do
       move ||= get_move
       break if board.valid_move?(move)
-      puts "Please enter a number between 1 and #{board.number_of_spaces} that has not already been taken"
+      @output.puts "Please enter a number between 1 and #{board.number_of_spaces} that has not already been taken"
       move = nil
     end
     move
   end
 
   def get_move
-    gets.chomp.to_i - 1
+    @input.gets.chomp.to_i - 1
   end
 end
--- spec/human_player_spec.rb
+++ spec/human_player_spec.rb
@@ -1,5 +1,6 @@
 require_relative '../lib/human_player'
 require_relative '../lib/board'
+require 'pry'
 
 RSpec.describe HumanPlayer do
   let(:board) { Board.new(Board::X_MARKER, Board::O_MARKER, 3) }
@@ -8,6 +9,20 @@ RSpec.describe HumanPlayer do
   describe "#find_move" do
     it 'confirms a valid move and returns it' do
       expect(human_player.find_move(board, 1)).to eq(1)
+    end
+  end
+
+  describe "#get_move" do
+    it 'returns -1 if passed a non-number string' do
+      human_player.input = StringIO.new('random string')
+      human_player.output = StringIO.new
+      expect(human_player.get_move).to eq(-1)
+    end
+
+    it "returns one less than the user's choice of move if it is (a string representation of) an integer" do
+      human_player.input = StringIO.new('5')
+      human_player.output = StringIO.new
+      expect(human_player.get_move).to eq(4)
     end
   end
 end

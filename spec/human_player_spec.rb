d65ecd066848ee0628d66d0df3c8620a877f077a
Matthew Higgins <matthew.alan.higgins@gmail.com>
2016-04-20 06:49:37 -0700

    Adds spec for human_player

---
+++ spec/human_player_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../lib/human_player'
+require_relative '../lib/board'
+
+RSpec.describe HumanPlayer do
+  let(:board) { Board.new(Board::X_MARKER, Board::O_MARKER, 3) }
+  let(:human_player) { HumanPlayer.new }
+
+  describe "#find_move" do
+    it 'returns a valid move' do
+      expect(human_player.find_move(board, 1)).to eq(1)
+    end
+  end
+
+end
